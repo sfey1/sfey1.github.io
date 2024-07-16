bb74cb98d21d34298464c3b71b03fd08fe3573f5
song <dusia05@naver.com>
2024-07-16 18:59:06 +0900

    hello world

---
+++ script/dataObject/mainData.js
@@ -0,0 +1,205 @@
+const mainData = [
+  {
+  src : "./imge/woody.png",
+  href:"./sitemap/woody/woody.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/mushroom.png",
+  href:"./sitemap/mushroom/mushroom.html",
+  alt:"아이모"
+},
+
+  {
+  src : "./imge/spore.png",
+  href:"./sitemap/spore/spore.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/island.png",
+  href:"./sitemap/island/island.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/dungeon1f.png",
+  href:"./sitemap/dungeon1f/dungeon1f.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/dungeon2f.png",
+  href:"./sitemap/dungeon2f/dungeon2f.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/dungeon3f.png",
+  href:"./sitemap/dungeon3f/dungeon3f.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/dungeon4f.png",
+  href:"./sitemap/dungeon4f/dungeon4f.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/dungeon4f.png",
+  href:"./sitemap/dungeon4f/dungeon4f.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/dungeon5f.png",
+  href:"./sitemap/dungeon5f/dungeon5f.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/pirate.png",
+  href:"./sitemap/pirate/pirate.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/temple.png",
+  href:"./sitemap/temple/temple.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/forestsiras.png",
+  href:"./sitemap/forest_siras/forest_siras.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/plains.png",
+  href:"./sitemap/plains/plains.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/forestlanos.png",
+  href:"./sitemap/forest_lanos/forest_lanos.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/kataru.png",
+  href:"./sitemap/kataru/kataru.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/desert.png",
+  href:"./sitemap/desert/desert.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/grassland.png",
+  href:"./sitemap/grassland/grassland.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/hotsand.png",
+  href:"./sitemap/hotsand/hotsand.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/ancient.png",
+  href:"./sitemap/ancient/ancient.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/westsky.png",
+  href:"./sitemap/westsky/westsky.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/eastsky.png",
+  href:"./sitemap/eastersky/eastersky.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/stone.png",
+  href:"./sitemap/stone/stone.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/unknown.png",
+  href:"./sitemap/unknown/unknown.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/fallen.png",
+  href:"./sitemap/fallen/fallen.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/polluted.png",
+  href:"./sitemap/polluted/polluted.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/swampdeath.png",
+  href:"./sitemap/swampdeath/swampdeath.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/labyrinth.png",
+  href:"./sitemap/laboratory/laboratory.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/laboratory.png",
+  href:"./sitemap/laboratory/laboratory.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/lslotemp.png",
+  href:"./sitemap/lslotemp/lslotemp.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/lunaF.png",
+  href:"./sitemap/lunaf/lunaf.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/altar.png",
+  href:"./sitemap/altar/altar.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/morphosis.png",
+  href:"./sitemap/morphosis/morphosis.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/garden.png",
+  href:"./sitemap/garden/garden.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/indun20.png",
+  href:"./sitemap/indun20/indun20.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/indun25.png",
+  href:"./sitemap/indun25/indun25.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/indun30.png",
+  href:"./sitemap/indun30/indun30.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/indun35.png",
+  href:"./sitemap/indun35/indun35.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/40.png",
+  href:"./sitemap/indun40/indun40.html",
+  alt:"아이모"
+},
+  {
+  src : "./imge/44.png",
+  href:"./sitemap/indun44/indun44.html",
+  alt:"아이모"
+},
+
+
+]
